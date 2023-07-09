c79f13af40ac6d8c7da18f9418053831cca38ae8
smallScholar <szq1232000@163.com>
2023-07-09 23:21:32 +0800

    二叉树完结

--- src/com/smallscholar/代码随想录/tree/ConvertBST.java
+++ src/com/smallscholar/代码随想录/tree/ConvertBST.java
@@ -7,4 +7,19 @@ package com.smallscholar.代码随想录.tree;
  * @describe 538. 把二叉搜索树转换为累加树
  */
 public class ConvertBST {
+
+    int sum = 0;
+
+    public TreeNode convertBST(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        convertBST(root.right);
+        sum += root.val;
+        root.val = sum;
+        convertBST(root.left);
+
+        return root;
+    }
+
 }
