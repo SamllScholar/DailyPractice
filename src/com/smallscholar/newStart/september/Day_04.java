69cd89b1f30a6e73a49758b098c7610e2e8de827
smallScholar <szq1232000@163.com>
2023-09-26 13:50:36 +0800

    序列化与反序列化二叉搜索树

---
+++ src/com/smallscholar/newStart/september/Day_04.java
@@ -0,0 +1,56 @@
+package com.smallscholar.newStart.september;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+/**
+ * @author smallscholar
+ * @date 2023/9/26 13:46
+ * @version 1.0
+ * @describe ...
+ */
+public class Day_04 {
+
+    public String serialize(TreeNode root) {
+        List<Integer> list = new ArrayList<Integer>();
+        postOrder(root, list);
+        String str = list.toString();
+        return str.substring(1, str.length() - 1);
+    }
+
+    public TreeNode deserialize(String data) {
+        if (data.isEmpty()) {
+            return null;
+        }
+        String[] arr = data.split(", ");
+        Deque<Integer> stack = new ArrayDeque<Integer>();
+        int length = arr.length;
+        for (int i = 0; i < length; i++) {
+            stack.push(Integer.parseInt(arr[i]));
+        }
+        return construct(Integer.MIN_VALUE, Integer.MAX_VALUE, stack);
+    }
+
+    private void postOrder(TreeNode root, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+        postOrder(root.left, list);
+        postOrder(root.right, list);
+        list.add(root.val);
+    }
+
+    private TreeNode construct(int lower, int upper, Deque<Integer> stack) {
+        if (stack.isEmpty() || stack.peek() < lower || stack.peek() > upper) {
+            return null;
+        }
+        int val = stack.pop();
+        TreeNode root = new TreeNode(val);
+        root.right = construct(val, upper, stack);
+        root.left = construct(lower, val, stack);
+        return root;
+    }
+
+}
---
+++ src/com/smallscholar/newStart/september/TreeNode.java
@@ -0,0 +1,22 @@
+package com.smallscholar.newStart.september;
+
+/**
+ * @author smallscholar
+ * @date 2023/9/26 13:48
+ * @version 1.0
+ * @describe ...
+ */
+public class TreeNode {
+
+    public TreeNode() {
+    }
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+}
